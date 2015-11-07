029e6295b9e2f2ba2708a53835afd98f17ea43fb
Ptitpaindepice <gabrielvautrin@gmail.com>
2015-11-07 12:46:50 +0100

    Récupération contenu d'un fichier ligne par ligne: done

---
+++ diff_windows/linked_list.c
@@ -0,0 +1,88 @@
+#include "linked_list.h"
+#include <stdlib.h>
+#include <stdio.h>
+/* Implements your functions here.*/
+
+t_maillon* new_maillon(int charLength){
+  t_maillon *t;
+  t = (t_maillon*)malloc(sizeof(t_maillon));
+  t->datas = malloc(sizeof(char)*charLength);
+  t->next = NULL;
+  printf("cr\212ation pointeur %p de valeur %s\n", t, t->datas);
+  return t;
+}
+
+unsigned int linked_list_size(t_maillon* t){
+  //printf("getSize\n");
+  if(!t) return 0;
+  unsigned int size = 1;
+  t_maillon* new_t;
+  new_t = t;
+  while(new_t->next != NULL){
+    size++;
+    new_t = new_t->next;
+  }
+  return size;
+}
+
+int linked_list_get_by_position(t_maillon* t, unsigned int pos, int* find){
+  //printf("ok1\n");
+  int i;
+  t_maillon* new_t;
+  new_t = t;
+  unsigned int size = linked_list_size(new_t);
+  if(pos < size && pos >= 0){
+    for(i = 0; i < pos; i++){
+      //printf("boucle\n");
+      new_t = new_t->next;
+    }
+    //printf("ok2\n");
+    *find = 1;
+    return new_t->datas;
+  }else{
+    *find = 0;
+    return 0;
+  }
+}
+
+int linked_list_get_by_value(t_maillon* t, int value){
+  t_maillon* new_t;
+  new_t = t;
+  int pos = 0;
+
+  while(new_t->datas != value){
+    pos++;
+    new_t = new_t->next;
+    if(new_t == NULL){
+      return (-1);
+    }
+  }
+  return pos;
+}
+
+void linked_list_display(t_maillon* t){
+  //printf("display\n");
+  t_maillon* new_t;
+  new_t = t;
+
+  do{
+
+  }while((new_t = new_t->next) != NULL);
+  printf("\n");
+  return;
+}
+
+void linked_list_free(t_maillon** t){
+  printf("free\n");
+  t_maillon** address = t;
+  t_maillon* new_t = *address;
+  t_maillon* t_remove;
+
+  while((t_remove = new_t)){
+    printf("lib\212ration dynamique %p de valeur %s et pointe sur %p\n", t_remove, t_remove->datas, t_remove->next);
+    free(t_remove->datas);
+    new_t = new_t->next;
+    free(t_remove);
+  }
+  return;
+}
--- diff_windows/main.c
+++ diff_windows/main.c
@@ -1,8 +1,113 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "linked_list.h"
 
-int main()
+void line_comp();
+void dealingWithArg(char*);
+void printOutput();
+void displayHelp();
+
+//GESTION DES ERREURS
+
+typedef struct file_parameters{
+  int lines_nbr;
+  int* nbr_char_per_line;
+  FILE* fp;
+}file_params;
+
+file_params* new_file_params();
+file_params* set_lines_nbr(file_params*, int nbr);
+int get_lines_nbr(file_params*, int nbr);
+file_params* set_nbr_char_per_line(file_params*, int nbr, int line);
+int get_nbr_char_per_line(file_params*, int line);
+
+int main(int argc, char* argv[])
 {
-    printf("Hello world!\n");
+    if(argc < 3){
+        printf("Nombre d 'arguments insuffisant");
+        return 1;
+    }
+
+    int lines_nbr1, lines_nbr2, nbr_char1, nbr_char2;
+    int i,j;
+
+
+    lines_nbr2 = lines_nbr1 = 1 ;
+    nbr_char1 = nbr_char2 = 0;
+
+    char **lines1, **lines2;
+    int *nbr_char_per_line1, *nbr_char_per_line2;
+    FILE *fp1, *fp2;
+
+    fp1 = fopen(argv[1], "r");
+    fp2 = fopen(argv[2], "r");
+
+    char c;
+
+    // COMPTE DU NOMBRE DE LIGNES ET STOCKAGE DS lines_nbr1
+    while((c = fgetc(fp1)) != EOF){
+      //printf(" ligne %d: ", lines_nbr1);
+      //printf(" char %c\n", c);
+      if(c == 10){
+         lines_nbr1++;
+      }
+    }
+
+    //printf("nbr lignes: %d\n", lines_nbr1);
+    lines1 = malloc(sizeof(char*)*lines_nbr1);
+    nbr_char_per_line1 = malloc(sizeof(int)*lines_nbr1);
+    rewind(fp1);
+
+    // COMPTE DU NOMBRE DE CARACTERE PAR LIGNE ET STOCKAGE DS nb_char_per_line
+    i = 0; //représente l'index de ligne
+
+    while((c = fgetc(fp1)) != EOF){
+      nbr_char1++;
+      if(c == 10){
+        nbr_char_per_line1[i] = ++nbr_char1;
+        //printf("nbr char ligne %d: %d\n",i, nbr_char1);
+        i++;
+        nbr_char1 = 0;
+      }
+    }
+    nbr_char_per_line1[i] = ++nbr_char1;
+    //printf("nbr char ligne %d: %d\n",i, nbr_char1);
+
+    rewind(fp1);
+
+    for(i = 0; i < lines_nbr1; i++){
+      //printf("i: %d\n", i);
+      lines1[i] = malloc(sizeof(char)*nbr_char_per_line1[i]);
+      for(j = 0; j < nbr_char_per_line1[i]-1; j++){
+        if((c = fgetc(fp1)) != EOF){
+          /*if(c == '\n')
+            printf("j: %d, char: \\n\n", j);
+          else
+            printf("j: %d, char: %c\n", j, c);*/
+          lines1[i][j] = c;
+        }
+      }
+      lines1[i][nbr_char_per_line1[i]-1] = '\0';
+
+      if(lines1[i][nbr_char_per_line1[i]-2])
+    }
+
+    for(i = 0; i < )
+
+
+    fclose(fp1);
+    fclose(fp2);
     return 0;
 }
+
+//FILE *tmpfile(void);
+//int setvbuf(FILE *stream, char* buf, int mode, size_t size);
+/*
+*   mode:   _IOFBF tamponnage complet
+*           _IOLBF tamponnage par lignes de fichier
+*           _IONBF ne provoque pas de tamponnage
+*/
+//int fseek ( FILE * stream, long int offset, int origin ); Reposition stream position indicator
+//long int ftell ( FILE * stream ); Get current position in stream
+//size_t fread ( void * ptr, size_t size, size_t count, FILE * stream ); Read block of data from stream
+//void rewind ( FILE * stream ); Set position of stream to the beginning
